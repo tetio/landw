144c510b44e024973bbdae359fe4f8a34328d09a
Sergi Maymi <smaymi.work@gmail.com>
2016-01-26 21:54:31 +0100

    fixed former merge errors

--- src/PlayState.ts
+++ src/PlayState.ts
@@ -23,28 +23,10 @@ module states {
         }
 
         update() {
-<<<<<<< HEAD
-=======
-            this.game.physics.arcade.collide(this.hero, this.platforms.platforms);
-            this.hero.body.velocity.x = 0;
-
-            if (this.cursors.up.isDown && this.hero.body.touching.down) {
-                this.hero.body.velocity.y = -350;
-            } else if (this.cursors.left.isDown) {
-                this.hero.body.velocity.x = -150;
-                this.hero.animations.play('left');
-            } else if (this.cursors.right.isDown) {
-                this.hero.body.velocity.x = 150;
-                this.hero.animations.play('right');
-            } else {
-                this.hero.frame = 4;
-            }
-
             // if (this.hero.body.velocity.x != 0 || this.hero.body.velocity.y != 0) {
             //     console.log("Hero(" + this.hero.body.position.x + ", " + this.hero.body.position.y + ")");
             // }
 
->>>>>>> d1efbdb1f1f0afe9b4dd2d80290e93f1a0252532
         }
 
 
